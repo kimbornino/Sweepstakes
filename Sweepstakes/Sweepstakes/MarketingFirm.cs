65733c9063712729071de068d6d7c3802bbb5746
Kimbornino <kimbornino@gmail.com>
2018-09-14 09:40:42 -0500

    Implemented dependency injection in the MarketingFirm class.

--- Sweepstakes/Sweepstakes/MarketingFirm.cs
+++ Sweepstakes/Sweepstakes/MarketingFirm.cs
@@ -8,6 +8,28 @@ namespace Sweepstakes
 {
     class MarketingFirm
     {
-        
+        //member variables
+        ISweepstakesManager sweepstakesManager;
+        //constructor
+
+        public MarketingFirm(ISweepstakesManager sweepstakesManager)
+        {
+            this.sweepstakesManager = sweepstakesManager;
+        }
+        public void ChooseStorageMethod()
+        {
+            Console.WriteLine("How would you like to store your contestant data?  Type Queue or Stack");
+            string dataStructureChoice = Console.ReadLine();
+
+            if (dataStructureChoice == "Queue")
+            {
+                //how to I designate which one to acitivate
+                SweepstakesQueueManager.AddIndividualContestantsToStructure();
+            }
+            else
+            {
+
+            }
+        }
     }
 }
